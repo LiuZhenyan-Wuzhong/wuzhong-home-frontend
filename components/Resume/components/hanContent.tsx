2593b04b71088518cdd2f877eb4e81bd44edbe42
LiuZhenyan-Wuzhong <cd750635968@sina.com>
2023-04-12 11:07:44 +0800

    update resume

--- components/Resume/components/hanContent.tsx
+++ components/Resume/components/hanContent.tsx
@@ -42,13 +42,16 @@ function EducationItem({
 }: EducationItemProps): JSX.Element {
   return (
     <div className="flex flex-col gap-1">
-      <div className="text-lg font-bold text-primary dark:text-primaryHover">{time}</div>
+      <div className="flex gap-4 items-end">
+        <div className="text-lg font-bold text-primary dark:text-primaryHover w-32">{time}</div>
+      </div>
+
       <div className="flex gap-4 items-end">
         <div className="text-xl font-bold">{university}</div>
         <div className="text-xl font-normal text-gray-400 dark:text-gray-500">{degree}</div>
-      </div>
-      <div className="text-xl font-normal text-gray-600 dark:text-gray-200">{course}</div>
 
+        {/* <div className="text-xl font-normal text-gray-600 dark:text-gray-200">{course}</div> */}
+      </div>
       <div className="text-xl font-normal text-gray-600 dark:text-gray-200">{score}</div>
     </div>
   );
@@ -131,7 +134,11 @@ function ExperienceItem({
         <div className="text-lg font-bold text-primary dark:text-primaryHover">{time}</div>
         <div className="text-xl font-semibold text-gray-600 dark:text-gray-200">{company}</div>
       </div>
-      <div className="text-xl font-normal text-gray-600 dark:text-gray-200">{work}</div>
+      {work.split('\n').map((sub, idx) => (
+        <div key={idx} className="text-xl font-normal text-gray-600 dark:text-gray-200">
+          {sub}
+        </div>
+      ))}
     </div>
   );
 }
@@ -146,7 +153,7 @@ interface SkillItemProps extends AllHTMLAttributes<HTMLDivElement> {
 function SkillItem({ children, skillType, skillName, description }: SkillItemProps): JSX.Element {
   return (
     <div className="flex gap-4 items-start">
-      <div className="flex flex-col gap-0">
+      <div className="flex flex-col gap-0 flex-shrink-0">
         <div className="text-lg font-bold text-gray-600 dark:text-gray-200">{skillType}</div>
         {/* <div className="text-xs font-normal text-gray-400 dark:text-gray-500">{skillName}</div> */}
       </div>
@@ -261,15 +268,15 @@ function HanContent(
 
       <div className="flex w-full h-[85%] gap-8 text-xl text-gray-600 dark:text-gray-200 font-semibold">
         <div id="left-bottom" className={clsx('flex w-[32%] flex-col h-full justify-between')}>
-          <div id="education" className={clsx('flex flex-col gap-6')}>
-            <Title>{'教育背景'}</Title>
+          <div id="education" className={clsx('flex flex-col gap-3')}>
+            <Title className="mb-2">{'教育背景'}</Title>
 
             <EducationItem
               time={'2021 - *2024'}
               university={'同济大学'}
               degree={'建筑学硕士'}
               course={'修读：人工智能导论 \\ 数字建筑学'}
-              score={'GPA: 4.7 成绩: 89.63/100' || undefined}
+              score={'GPA: 4.7  成绩: 89.63/100' || undefined}
             />
 
             <EducationItem
@@ -277,72 +284,85 @@ function HanContent(
               university={'浙江大学'}
               degree={'建筑学学士'}
               course={'修读：参数化设计 \\ 建筑机器人'}
-              score={'GPA: 4.12 排名: 8/84' || undefined}
+              score={'GPA: 4.12  排名: 8/84' || undefined}
             />
           </div>
 
-          <div id="experiance" className={clsx('flex flex-col gap-6')}>
-            <Title>{'实践经历'}</Title>
-
-            <div className={clsx('flex flex-col flex-1 flex-grow gap-6')}>
-              <ExperienceItem
-                time="9.2017 - 6.2019"
-                company={'浙江大学'}
-                work={'担任四星社团"校友联络协会"会长'}
-              />
-              <ExperienceItem
-                time="9.2016 - 6.2021"
-                company={'浙江大学'}
-                work={'参与志愿活动，荣获四星志愿者'}
-              />
-              <ExperienceItem
-                time="6.2021 - 9.2021"
-                company={'群核科技'}
-                work={
-                  '作为科研算法实习生，负责手绘草图三维重建相关文献阅读和复现。学习了深度学习技术以及git\\linux等工作技能。将技术经验应用到了“极智未来·建筑黑客马拉松”竞赛作品中，最终入围，排名第八。'
-                }
-              />
-              {/* <ExperienceItem
-                time="1.2022 - 3.2022"
-                company={'同济大学'}
-                work={'极智未来·建筑黑客马拉松 前八'}
-              /> */}
-            </div>
-          </div>
-
-          <div id="awards" className={clsx('flex flex-col gap-6')}>
-            <Title>{'获奖情况'}</Title>
+          <div id="awards" className={clsx('flex flex-col gap-3')}>
+            <Title className="mb-2">{'获奖情况'}</Title>
 
             <AwardsItem time={'6.2017'} nameList={['浙江大学优秀学生三等奖学金']} />
 
-            <AwardsItem time={'6.2018'} nameList={['浙江大学学业二等奖学金', '浙江省政府奖学金']} />
+            <AwardsItem
+              time={'6.2018'}
+              nameList={['浙江大学学业二等奖学金', '浙江省政府奖学金', '浙江大学四星志愿者']}
+            />
 
             <AwardsItem
               time={'6.2019'}
               nameList={['浙江大学优秀学生三等奖学金', '坚朗二等奖学金']}
             />
 
-            {/* <AwardsItem time={'2.2022'} nameList={[t('awards.item4.nameList.name1')]} /> */}
+            <AwardsItem time={'2.2022'} nameList={['极智未来·建筑黑客马拉松 前八']} />
+          </div>
+
+          <div id="professional-skills" className="flex flex-col gap-3">
+            <Title className="mb-2">{'专业技能'}</Title>
+            <SkillItem
+              skillType={'前端基础'}
+              skillName={'HTML\\CSS\\JavaScript'}
+              description={'能够熟练使用 HTML \\ CSS \\ JavaScript等基础语言'}
+            />
+            <SkillItem
+              skillType={'前端框架'}
+              skillName={'React\\Nextjs'}
+              description={'擅长的框架为React，了解NextJS并开发过实际项目'}
+            />
+            <SkillItem
+              skillType={'代码规范'}
+              skillName={'TypeScript\\eslint'}
+              description={'熟悉TypeScript，习惯配合eslint开发以减少错误'}
+            />
+            <SkillItem
+              skillType={'构建工具'}
+              skillName={'Vite'}
+              description={'熟悉Vite等构建工具'}
+            />
+            <SkillItem
+              skillType={'后端技术'}
+              skillName={'nodejs\\python'}
+              description={'了解Nodejs\\Python等后端技术，了解数据库使用'}
+            />
+            <SkillItem
+              skillType={'设计基础'}
+              skillName={'PS\\AI\\figma'}
+              description={'能使用PS\\AI\\figma等设计工具，与设计师高效对接'}
+            />
+            <SkillItem
+              skillType={'桌面开发'}
+              skillName={'Electron'}
+              description={'能够使用Electron进行桌面端开发'}
+            />
           </div>
         </div>
 
-        <div id="right-bottom" className={clsx('flex w-[68%] flex-col justify-between gap-12')}>
-          <div id="profile" className={clsx('flex flex-col gap-6')}>
+        <div id="right-bottom" className={clsx('flex w-[68%] flex-col justify-between flex-grow')}>
+          <div id="profile" className={clsx('flex flex-col gap-5')}>
             <Title>{'个人简介'}</Title>
 
             <p className="text-xl font-normal text-gray-600 dark:text-gray-200">
               {
-                '刘真言，1999年生于湖北。本科毕业于浙江大学，研究生就读于同济大学(2024年毕业)。自学前端课程并参与多个项目的开发，有全栈开发经历。具有较强的学习能力，对于前端开发技术有热情，热爱学习新技术。'
+                '刘真言，1999年生于湖北。本科毕业于浙江大学(建筑学)，研究生就读于同济大学(建筑学,2024年春毕业)。学习能力强，自学前端，参与多个项目的开发，有全栈开发经历。'
               }
             </p>
-            <p className="text-xl font-normal text-gray-600 dark:text-gray-200">
+            {/* <p className="text-xl font-normal text-gray-600 dark:text-gray-200">
               {
                 '本科期间曾担任社团负责人，具有较强的责任心；多次参与志愿活动并获得四星志愿者，拥有较强的同理心；多次团队建筑设计任务，具有良好的沟通能力与团队协作能力。'
               }
-            </p>
+            </p> */}
           </div>
 
-          <div id="project" className={clsx('flex flex-col gap-6')}>
+          <div id="project" className={clsx('flex flex-col gap-5')}>
             <Title>{'参与项目'}</Title>
 
             <ProjectItem
@@ -360,7 +380,7 @@ function HanContent(
                 'MySQL'
               ]}
               introduction={
-                '一个基于Nextjs框架对建筑图纸进行建模和规范审核的项目，使用Radix-UI组件库配合tailwindcss进行布局。几何显示通过Three.js实现。\n本人负责部分交互逻辑，Three.js几何操作，后端，数据库，建模逻辑。'
+                '本项目是一个Web建筑图纸自动建模与规范审核工具。需实现图纸上传、用户项目管理、模型三维显示、自动审核规范等功能。\n我负责使用NextJS框架配合Radix-UI组件库和tailwindcss进行页面布局，使用Three.js实现模型显示，使用Django和MySQL搭建RESTful API服务器提供用户和项目管理功能，通过Python实现图纸的三维建模。'
               }
             />
 
@@ -370,7 +390,7 @@ function HanContent(
               // description={t('projects.item2.description')}
               stackList={['Electron', 'Vite', 'React', 'ChatGPT-API', 'TypeScript']}
               introduction={
-                '一款基于Electron和Vite构建的桌面词汇翻译软件，翻译逻辑通过调用ChatGPT API实现。前端界面与交互采用React框架。\n本人独立完成项目策划、设计、编码任务。'
+                '本项目是一款桌面划词翻译软件。需实现自动翻译用户所选字段的功能。\n由我个人进行设计与开发，使用了Electron构建桌面软件并配合Vite实现高效打包，使用了React进行界面布局以及实现交互，调用ChatGPT API实现翻译逻辑。最终项目实现了预期功能。'
               }
             />
 
@@ -379,49 +399,44 @@ function HanContent(
               name={'Sketch2Param'}
               // description={t('projects.item3.description')!}
               stackList={['Pytorch', 'HTML', 'CSS', 'JavaScript', 'Three.js']}
+              /**
+               *
+               *
+               */
               introduction={
-                '使用HTML\\CSS\\JavaScript实现的"手绘草图参数化建模"项目，重建逻辑使用基于Pytorch的深度学习模型(Res-Net)，几何显示使用Three.js库。\n由本人独立完成项目编码工作。'
+                '本项目是一个Web端手绘草图参数化建模工具。需实现基于用户上传的草图预测参数化模型参数的功能。\n由本人主导完成项目编码工作，使用HTML\\CSS\\JavaScript实现了用户界面，通过Pytorch搭建并训练了深度学习模型(Res-Net)以实现对参数化模型的预测，通过Three.js实现了参数化模型的显示并提供了用户调参接口。该项目入围了"极智未来·建筑黑客马拉松"竞赛，最终进入前八。'
               }
             />
           </div>
 
-          <div id="professional-skills" className="flex flex-col gap-3">
-            <Title className="mb-6">{'专业技能'}</Title>
-            <SkillItem
-              skillType={'前端基础'}
-              skillName={'HTML\\CSS\\JavaScript'}
-              description={'能够熟练使用 HTML \\ CSS \\ JavaScript等基础语言，'}
-            />
-            <SkillItem
-              skillType={'前端框架'}
-              skillName={'React\\Nextjs'}
-              description={'擅长的前端框架为React，能使用Nextjs并开发过实际项目。'}
-            />
-            <SkillItem
-              skillType={'代码规范'}
-              skillName={'TypeScript\\eslint'}
-              description={'熟悉TypeScript语法，并习惯配合eslint开发以减少错误。'}
-            />
-            <SkillItem
-              skillType={'构建工具'}
-              skillName={'Vite'}
-              description={'熟悉Vite等构建工具。'}
-            />
-            <SkillItem
-              skillType={'桌面开发'}
-              skillName={'Electron'}
-              description={'能够使用Electron进行桌面端开发。'}
-            />
-            <SkillItem
-              skillType={'后端技术'}
-              skillName={'nodejs\\python'}
-              description={'了解Nodejs\\Python(Django)等后端技术，了解数据库的使用。'}
-            />
-            <SkillItem
-              skillType={'设计基础'}
-              skillName={'PS\\AI\\figma'}
-              description={'能使用PS\\AI\\figma等设计工具，与设计师高效对接，较好还原设计。'}
-            />
+          <div id="experiance" className={clsx('flex flex-col gap-5')}>
+            <Title>{'实践经历'}</Title>
+
+            <div className={clsx('flex flex-col flex-1 flex-grow gap-3')}>
+              <ExperienceItem
+                time="6.2021 - 9.2021"
+                company={'群核科技'}
+                work={
+                  '前沿技术研究院是群核科技负责研究前沿算法和技术的部门。我担任科研算法实习生，主要负责手绘草图三维重建相关文献阅读和复现。\n在实习中，我兼顾了论文阅读、成果和算法实操，对复现的深度学习模型进行试验。最终完成了两篇文章的算法复现，帮助部门总结了模型训练经验。此过程中我也锻炼了自身的深度学习技术以及git\\linux等工作技能。'
+                }
+              />
+              <ExperienceItem
+                time="9.2017 - 6.2019"
+                company={'浙江大学'}
+                work={'担任四星社团"校友联络协会"会长，有较强责任心与团队协作能力。'}
+              />
+              {/* <ExperienceItem
+                time="9.2016 - 6.2021"
+                company={'浙江大学'}
+                work={'参与志愿活动，荣获四星志愿者'}
+              /> */}
+
+              {/* <ExperienceItem
+                time="1.2022 - 3.2022"
+                company={'同济大学'}
+                work={'极智未来·建筑黑客马拉松 前八'}
+              /> */}
+            </div>
           </div>
         </div>
       </div>
